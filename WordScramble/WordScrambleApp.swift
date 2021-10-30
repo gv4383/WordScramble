0321b56f76ba92831ad25168d37e142ec5d79870
gv4383 <gregory.vargas18@gmail.com>
2021-10-29 23:42:25 -0500

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Goyo Vargas on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
